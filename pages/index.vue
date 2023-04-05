96b64fa422d3248adc66845e1a888e09bde0096a
Export <106684380+Exportqq@users.noreply.github.com>
2023-04-05 17:48:22 +0300

    first commit

--- pages/index.vue
+++ pages/index.vue
@@ -71,37 +71,12 @@ import AOS from 'aos'
 import 'aos/dist/aos.css'
 
 export default {
-  head() {
-    return {
-      script: [
-        {
-          src: 'https://yandex.ru/ads/system/context.js'
-        }
-      ],
-    }
-  },
   data() {
     return {
     }
   },
   mounted() {
     AOS.init({ });
-    (function(w, d, n, s, t) {
-        w[n] = w[n] || [];
-        w[n].push(function() {
-          Ya.Context.AdvManager.render({
-            blockId: "R-A-2282880-1",
-            renderTo: "yandex_rtb_R-A-2282880-1",
-            async: true
-          });
-        });
-        t = d.getElementsByTagName("script")[0];
-        s = d.createElement("script");
-        s.type = "text/javascript";
-        s.src = "//an.yandex.ru/system/context.js";
-        s.async = true;
-        t.parentNode.insertBefore(s, t);
-      })(window, document, "yandexContextAsyncCallbacks");
   },
 }
 </script>
